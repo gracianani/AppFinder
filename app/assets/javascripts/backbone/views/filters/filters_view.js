ee66c5b38ee5e37c2fa4e65099d8ed4d465114f9
Yu Hong <hello.hyu@gmail.com>
2012-11-23 14:19:35 +0800

    filter dropdown

--- app/assets/javascripts/backbone/views/filters/filters_view.js
+++ app/assets/javascripts/backbone/views/filters/filters_view.js
@@ -3,19 +3,22 @@ AppFinder.Views.Filters = AppFinder.Views.Filters || {};
 AppFinder.Views.Filters.FiltersView = Backbone.View.extend({
   template: JST["backbone/templates/filters/search"],
   events: {
-
+	  "click .filter-btn" : 'toggleFilterBtn'
   },
   tagName : "div",
   className : "container",
   id: "filter",
+  toggleFilterBtn:function(e){
+	  //$(e.currentTarget).toggleClass('over');
+  },
   render: function(){
     console.log(this.options.filters.toJSON());
   	$(this.el).html(this.template(this.options.filters.toJSON()[0]));
 	setTimeout( function (){
 		
 		$(".chzn-select").chosen();
-		$('.dropdown-toggle').dropdown();
-		
+		//$('.dropdown-toggle').dropdown();
+		$('.smlDropdown').smlDropdown();
 	}, 0);
   	
   	return this;
--- app/assets/javascripts/vendor/smlDropdown.js
+++ app/assets/javascripts/vendor/smlDropdown.js
@@ -1,28 +1,26 @@
 (function ($, window) {
-  $.fn.responsiveSlides = function (options) {
+  $.fn.smlDropdown = function (options) {
 
     // Default settings
     var settings = $.extend({
       "auto": true,             // Boolean: Animate automatically, true or false
-      "speed": 1000,            // Integer: Speed of the transition, in milliseconds
-      "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
-      "pager": false,           // Boolean: Show pager, true or false
-      "nav": false,             // Boolean: Show navigation, true or false
-      "random": false,          // Boolean: Randomize the order of the slides, true or false
-      "pause": false,           // Boolean: Pause on hover, true or false
-      "pauseControls": false,   // Boolean: Pause when hovering controls, true or false
-      "prevText": "Previous",   // String: Text for the "previous" button
-      "nextText": "Next",       // String: Text for the "next" button
-      "maxwidth": "",           // Integer: Max-width of the slideshow, in pixels
-      "controls": "",           // Selector: Where controls should be appended to, default is after the <ul>
-      "namespace": "rslides",   // String: change the default namespace used
-      before: function () {},   // Function: Before callback
-      after: function () {}     // Function: After callback
+      "namespace": "smldropdown",   // String: change the default namespace used
+
     }, options);
 
     return this.each(function () {
-    
-    	
+      // Index for namespacing
+      i++;
+
+      var $this = $(this),
+      // Namespacing
+        namespace = settings.namespace,
+        namespaceIdx = namespace + i;
+    	$this.bind('click',function(){
+	    	$this.toggleClass('over');
+	    	console.log('here');
+	    	$this.find('.smlDropdown-menu').toggle();
+    	});
     });
  };
 })(jQuery,this);
