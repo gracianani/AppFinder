6096b392bdd4a1da91095494ebb0e0241f9492f2
yaqi zhao <rainy_star_2001@hotmail.com>
2012-11-26 09:57:48 -0600

    loading

---
+++ app/assets/javascripts/backbone/views/loading_view.js
@@ -0,0 +1,27 @@
+AppFinder.Views.WaterfallApps = AppFinder.Views.WaterfallApps || {};
+
+LoadingStates = [".", "..", "..."];
+Tick = 1;
+LoadingInterval = 0;
+
+AppFinder.Views.LoadingView = Backbone.View.extend({
+	template: JST["backbone/templates/loading"],
+  	tagName : "div",
+  	id: "loading",
+  	
+  	render: function() {
+  		$(this.el).html(this.template( ));
+  		 LoadingInterval = setInterval( function() {
+  		 	if($('#loading').length==0) {
+  		 		window.clearInterval(LoadingInterval);
+  		 
+  		 	}
+  		 	$('#loading .loading-msg').html("Loading" + LoadingStates[Tick%3] );
+  		 	Tick=Tick+1;
+  		 }, 500);
+    	return this;
+  	},
+	appendDot: function() {
+		
+	}
+});
