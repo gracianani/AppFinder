77a16223cb74f94c3a7ff03cea53516ef2bfacd1
yaqi zhao <rainy_star_2001@hotmail.com>
2012-11-30 01:38:36 -0600

    video

--- app/assets/javascripts/backbone/views/waterfall_apps/show_view.js
+++ app/assets/javascripts/backbone/views/waterfall_apps/show_view.js
@@ -6,13 +6,30 @@ AppFinder.Views.WaterfallApps.ShowView = Backbone.View.extend ({
   className: "show",
   tagName : "div",
   initialize: function ()  {
+  	this.players=[];
+  	this.videoStatus = []; // 0 => initializing, 1 => ready
   	this.model.on('change',this.render,this);
   },
   events: {
     "click .expand" : "toggleDesc",
     "click .folded" : "toggleDesc",
     "mouseenter .btn-install" : "toggleInstallText",
-    "mouseleave .btn-install": "toggleInstallText"  },
+    "mouseleave .btn-install": "toggleInstallText",
+    "click #lnk_screenshots" : "showScreenShots",
+    "click #lnk_videos" : "showVideos"
+  },
+  showScreenShots : function( e) {
+  	e.preventDefault();
+  	$("#flexslider").show( function() {
+  		$("#video_flexslider").hide();
+  	})
+  },
+  showVideos : function( e) {
+  	e.preventDefault();
+  	$("#video_flexslider").show( function() {
+  		$("#flexslider").hide();
+  	})
+  },
   toggleDesc: function(e) {
   	  e.preventDefault();
 	  this.$el.find('.app-long-description').toggle();
@@ -44,25 +61,75 @@ AppFinder.Views.WaterfallApps.ShowView = Backbone.View.extend ({
 		});
 		
     },0);
-    
+    var that = this;  
     setTimeout(function() {
-    	$(".rslides").responsiveSlides({
-	  auto: true,             // Boolean: Animate automatically, true or false
-	  speed: 1000,            // Integer: Speed of the transition, in milliseconds
-	  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-	  pager: true,           // Boolean: Show pager, true or false
-	  nav: true,             // Boolean: Show navigation, true or false
-	  random: false,          // Boolean: Randomize the order of the slides, true or false
-	  pause: false,           // Boolean: Pause on hover, true or false
-	  pauseControls: false,   // Boolean: Pause when hovering controls, true or false
-	  prevText: "Previous",   // String: Text for the "previous" button
-	  nextText: "Next",       // String: Text for the "next" button
-	  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
-	  controls: "",           // Selector: Where controls should be appended to, default is after the 'ul'
-	  namespace: "rslides",   // String: change the default namespace used
-	  before: function(){},   // Function: Before callback
-	  after: function(){}     // Function: After callback
-	});}, 2000);
+    	var videos = that.model.get('videos');
+    	for(i = 0; i < videos.length; i=i+1) {
+    		var playerid="player"+i;
+    		that.videoStatus[i]='0';
+    		if(videos[i].video_type=="youtube") {
+	    		that.players[i]  = new YT.Player(playerid, {
+		      		height: '397',
+		      		width: '728',
+		      	    videoId: videos[i].video_id,
+		      	    events: {
+		      	    	'onReady' : function() { }
+		      	    }
+	    		});
+    		}
+    		else if(videos[i].video_type=="vimeo") {
+    			// render iframe
+    			var holder =$(that.el).find("#"+playerid);
+    			var player = '<iframe id="' + playerid+ 
+    			'" src="http://player.vimeo.com/video/'+ videos[i].video_id +
+    			'?api=1&amp;player_id='+ playerid + 
+    			'" width="728" height="397" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+    			
+    			holder.after(player);
+    			holder.remove();
+    			that.players[i] = $f(document.getElementById(playerid));
+				that.players[i].addEvent('ready', function() {
+				});
+    		}
+    	}
+    });
+    setTimeout(function() {
+    	
+    	
+    	
+	    $("#flexslider")
+	    .flexslider({
+			animation: "slide",
+			useCSS: false,
+			animationLoop: false,
+			smoothHeight: true,
+			animationSpeed: 300,
+			animationLoop: false
+	    });
+	    
+	    $("#video_flexslider").fitVids().flexslider({
+			animation: "fade",
+			useCSS: false,
+			animationLoop: false,
+			smoothHeight: false,
+			animationSpeed: 300,
+			before: function(slider){
+				var videos = that.model.get('videos');
+				for(i=0;i<that.players.length; i=i+1) {
+					if(that.players[i]!=null && typeof(that.players[i]) != 'undefined'){
+		        		if(videos[i].video_type == "vimeo") {
+		        			that.players[i].api('pause');
+		        		}
+		        		else if(videos[i].video_type == "youtube") {
+		        			if(typeof(that.players[i].stopVideo) != 'undefined') 
+		        			    that.players[i].stopVideo();
+		        		}
+		        	}
+		        }
+	      	}
+	    });
+  
+  }, 2000);
     return this;
   }
     
