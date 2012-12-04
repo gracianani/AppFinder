14e62875592d5ff81994ac2257cea17c0c3056ec
Yu Hong <hello.hyu@gmail.com>
2012-12-04 21:30:33 +0800

    app details

--- app/assets/javascripts/backbone/views/developer/show_view.js
+++ app/assets/javascripts/backbone/views/developer/show_view.js
@@ -11,7 +11,17 @@ AppFinder.Views.Developer.ShowView = Backbone.View.extend ({
   },
   render: function() {
   	$(this.el).html(this.template(this.model.toJSON()));
-
+  	$el = $(this.el);
+  	var ratingStr = this.model.get('app_avg_score');
+    var ratings = parseFloat(ratingStr);
+    if ( ratingStr.indexOf('+') > 0 ) {
+	    ratings += 0.5
+    }
+    $el.find('.raty').raty({
+		'readOnly':true,
+		'space':false,
+		'score':ratings	    
+    });
    return this;
 
   }
