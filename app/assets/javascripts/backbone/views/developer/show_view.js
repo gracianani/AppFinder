74be442ae7b68a6a9b6cf0a2561e0cfcd85379e6
Yu Hong <hello.hyu@gmail.com>
2012-12-05 15:56:15 +0800

    detail

--- app/assets/javascripts/backbone/views/developer/show_view.js
+++ app/assets/javascripts/backbone/views/developer/show_view.js
@@ -9,9 +9,18 @@ AppFinder.Views.Developer.ShowView = Backbone.View.extend ({
   	 this.model.on('change',this.render,this);
 
   },
+  events : {
+	  'click .smlDev':'clickSimilarDev'
+  },
+  clickSimilarDev: function(e){
+	  var href = $(e.currentTarget).attr('data-href');
+	  window.router.navigate(href);
+	 
+  },
   render: function() {
   	$(this.el).html(this.template(this.model.toJSON()));
   	$el = $(this.el);
+  	
   	var ratingStr = this.model.get('app_avg_score');
     var ratings = parseFloat(ratingStr);
     if ( ratingStr.indexOf('+') > 0 ) {
@@ -21,6 +30,20 @@ AppFinder.Views.Developer.ShowView = Backbone.View.extend ({
 		'readOnly':true,
 		'space':false,
 		'score':ratings	    
+    });
+    
+    $el.find('.app-raty,.smlDev-raty').each(function(){
+    	ratingStr = $(this).attr('data-rating');
+    	ratings = parseFloat(ratingStr);
+    	if ( ratingStr.indexOf('+') > 0 ) {
+	    	ratings += 0.5
+	    }
+	    $(this).raty({
+			'readOnly':true,
+			'space':false,
+			'score':ratings			    
+	    });
+    	
     });
    return this;
 
